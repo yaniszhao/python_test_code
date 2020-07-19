ae70ae60fc03f1f5d7f211b2c9212c08cd64b8eb
yaniszhao <yaniszhao@hotmail.com>
2020-07-19 15:16:14 +0800

    通过os.rename修改文件名去掉空格

--- src/rename.py
+++ src/rename.py
@@ -8,3 +8,14 @@ for file in li:
         cmd = "mv %s %s"%(file,file.replace(".mp4", ".m"))
         print cmd
 #        os.system(cmd)
+
+#通过os.rename修改文件名去掉空格
+rule = ' '
+li = os.listdir(os.getcwd())
+for file in li:
+    if re.search(rule,file):
+        #cmd = "mv %s %s"%(file,file.replace(" ", "_"))
+        #print cmd
+        #os.system(cmd)
+	os.rename(file,file.replace(" ", "_"))
+
